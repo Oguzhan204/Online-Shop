9c9b9d8b70debe0036ba5f9a50088d7972b4c31d
Oguzhan204 <oguzhan.atmaca (at) web.de>
2014-01-11 15:50:41 +0100

    123

--- shop/src/main/java/de/shop/kundenverwaltung/rest/KundeResource.java
+++ shop/src/main/java/de/shop/kundenverwaltung/rest/KundeResource.java
@@ -2,6 +2,7 @@ package de.shop.kundenverwaltung.rest;
 
 import static de.shop.util.Constants.ADD_LINK;
 import static de.shop.util.Constants.FIRST_LINK;
+import static de.shop.util.Constants.KEINE_ID;
 import static de.shop.util.Constants.LAST_LINK;
 import static de.shop.util.Constants.LIST_LINK;
 import static de.shop.util.Constants.REMOVE_LINK;
@@ -11,12 +12,17 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 import static javax.ws.rs.core.MediaType.TEXT_XML;
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 
+import java.lang.invoke.MethodHandles;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 import javax.ws.rs.Consumes;
@@ -35,34 +41,43 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.hibernate.validator.constraints.Email;
+import org.jboss.logging.Logger;
+
+import com.google.common.base.Strings;
 
 import de.shop.bestellverwaltung.domain.Bestellung;
 import de.shop.bestellverwaltung.rest.BestellungResource;
 import de.shop.bestellverwaltung.service.BestellungService;
 import de.shop.kundenverwaltung.domain.Kunde;
+import de.shop.kundenverwaltung.domain.Adresse;
 import de.shop.kundenverwaltung.service.KundeService;
+import de.shop.kundenverwaltung.service.KundeService.FetchType;
+import de.shop.kundenverwaltung.service.KundeService.OrderType;
 import de.shop.util.interceptor.Log;
-import de.shop.util.rest.NotFoundException;
 import de.shop.util.rest.UriHelper;
 
+
+/**
+ * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
+ */
 @Path("/kunden")
 @Produces({ APPLICATION_JSON, APPLICATION_XML + ";qs=0.75", TEXT_XML + ";qs=0.5" })
 @Consumes
 @RequestScoped
+@Transactional
 @Log
 public class KundeResource {
+	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
+	private static final String VERSION = "1.0";
+	
 	public static final String KUNDEN_ID_PATH_PARAM = "kundeId";
 	public static final String KUNDEN_NACHNAME_QUERY_PARAM = "nachname";
 	public static final String KUNDEN_PLZ_QUERY_PARAM = "plz";
 	public static final String KUNDEN_EMAIL_QUERY_PARAM = "email";
 	
-	private static final String NOT_FOUND_ID = "kunde.notFound.id";
-	//private static final String NOT_FOUND_NACHNAME = "kunde.notFound.nachname";
-	//private static final String NOT_FOUND_ALL = "kunde.notFound.all";
-	
-	@Context
-	private UriInfo uriInfo;
-	
+    @Context
+    private UriInfo uriInfo;
+    
 	@Inject
 	private KundeService ks;
 	
@@ -76,38 +91,39 @@ public class KundeResource {
 	private UriHelper uriHelper;
 	
 	@GET
-	@Produces({ TEXT_PLAIN, APPLICATION_JSON })
+	@Produces({ TEXT_PLAIN, APPLICATION_JSON + ";qs=0.75" })
 	@Path("version")
 	public String getVersion() {
-		return "1.0";
+		return VERSION;
 	}
 	
+	/**
+	 * Mit der URL /kunden/{id} einen Kunden ermitteln
+	 * @param id ID des Kunden
+	 * @return Objekt mit Kundendaten, falls die ID vorhanden ist
+	 */
 	@GET
 	@Path("{" + KUNDEN_ID_PATH_PARAM + ":[1-9][0-9]*}")
 	public Response findKundeById(@PathParam(KUNDEN_ID_PATH_PARAM) Long id) {
-		final Kunde kunde = ks.findKundeById(id);
-		if (kunde == null) {
-			throw new NotFoundException(NOT_FOUND_ID, id);
-		}
-		
+		final Kunde kunde = ks.findKundeById(id, FetchType.NUR_KUNDE);
 		setStructuralLinks(kunde, uriInfo);
+	
+		final Response response = Response.ok(kunde)
+                                          .links(getTransitionalLinks(kunde, uriInfo))
+                                          .build();
 
-		return Response.ok(kunde)
-			           .links(getTransitionalLinks(kunde, uriInfo))
-			           .build();
+		return response;
 	}
 	
 	public void setStructuralLinks(Kunde kunde, UriInfo uriInfo) {
-		// URI fuer Bestellungen setzen
-		final URI uri = getUriBestellungen(kunde, uriInfo);
-		kunde.setBestellungenUri(uri);
+		kunde.setBestellungenUri(getUriBestellungen(kunde, uriInfo));
 	}
 	
 	private URI getUriBestellungen(Kunde kunde, UriInfo uriInfo) {
 		return uriHelper.getUri(KundeResource.class, "findBestellungenByKundeId", kunde.getId(), uriInfo);
 	}
 	
-	private Link[] getTransitionalLinks(Kunde kunde, UriInfo uriInfo) {
+	public Link[] getTransitionalLinks(Kunde kunde, UriInfo uriInfo) {
 		final Link self = Link.fromUri(getUriKunde(kunde, uriInfo))
 	                          .rel(SELF_LINK)
 	                          .build();
@@ -117,8 +133,8 @@ public class KundeResource {
 		                      .build();
 
 		final Link add = Link.fromUri(uriHelper.getUri(KundeResource.class, uriInfo))
-		                     .rel(ADD_LINK)
-		                     .build();
+                             .rel(ADD_LINK)
+                             .build();
 		
 		final Link update = Link.fromUri(uriHelper.getUri(KundeResource.class, uriInfo))
 			                    .rel(UPDATE_LINK)
@@ -127,39 +143,45 @@ public class KundeResource {
 		final Link remove = Link.fromUri(uriHelper.getUri(KundeResource.class, "deleteKunde", kunde.getId(), uriInfo))
 		                        .rel(REMOVE_LINK)
 		                        .build();
-		
+
 		return new Link[] { self, list, add, update, remove };
 	}
-
 	
 	public URI getUriKunde(Kunde kunde, UriInfo uriInfo) {
 		return uriHelper.getUri(KundeResource.class, "findKundeById", kunde.getId(), uriInfo);
 	}
-	
+
+
+	/**
+	 * Mit der URL /kunden werden alle Kunden ermittelt oder
+	 * mit kundenverwaltung/kunden?nachname=... diejenigen mit einem bestimmten Nachnamen.
+	 * @param nachname Nachname der gesuchten Kunden
+	 * @return Collection mit den gefundenen Kundendaten
+	 */
 	@GET
 	public Response findKunden(@QueryParam(KUNDEN_NACHNAME_QUERY_PARAM)
-   	                           @Pattern(regexp = Kunde.NACHNAME_PATTERN, message = "{kunde.nachname.pattern}")
-							   String nachname,
-							   @QueryParam(KUNDEN_EMAIL_QUERY_PARAM)
-   	                           @Email(message = "{kunde.email}")
-							   String email,
-							   @QueryParam(KUNDEN_PLZ_QUERY_PARAM)
-   	                           @Pattern(regexp = "\\d{5}", message = "{adresse.plz}")
-							   String plz) {
+                               @Pattern(regexp = Kunde.NACHNAME_PATTERN, message = "{kunde.nachname.pattern}")
+	                           String nachname,
+                               @QueryParam(KUNDEN_PLZ_QUERY_PARAM)
+                               @Pattern(regexp = "\\d{5}", message = "{adresse.plz}")
+                               String plz,
+                               @QueryParam(KUNDEN_EMAIL_QUERY_PARAM)
+                               @Email(message = "{kunde.email}")
+                               String email) {
 		List<? extends Kunde> kunden = null;
 		Kunde kunde = null;
-		if (nachname != null) {
-			kunden = ks.findKundenByNachname(nachname);
+		// TODO Mehrere Query-Parameter koennen angegeben sein
+		if (!Strings.isNullOrEmpty(nachname)) {
+			kunden = ks.findKundenByNachname(nachname, FetchType.NUR_KUNDE);
 		}
-		else if (email != null) {
+		else if (!Strings.isNullOrEmpty(plz)) {
+			kunden = ks.findKundenByPLZ(plz);
+		}
+		else if (!Strings.isNullOrEmpty(email)) {
 			kunde = ks.findKundeByEmail(email);
 		}
-		else if (plz != null) {
-			// TODO Beispiel fuer ein TODO bei fehlender Implementierung
-			throw new RuntimeException("Suche nach PLZ noch nicht implementiert");
-		}
 		else {
-			kunden = ks.findAllKunden();
+			kunden = ks.findAllKunden(FetchType.NUR_KUNDE, OrderType.ID);
 		}
 		
 		Object entity = null;
@@ -168,7 +190,7 @@ public class KundeResource {
 			for (Kunde k : kunden) {
 				setStructuralLinks(k, uriInfo);
 			}
-			// FIXME JDK 8 hat Lambda-Ausdruecke, aber Proxy-Klassen von Weld funktionieren noch nicht mit Lambda-Ausdruecken
+			// FIXME JDK 8 hat Lambda-Ausdruecke
 			//kunden.parallelStream()
 			//      .forEach(k -> setStructuralLinks(k, uriInfo));
 			entity = new GenericEntity<List<? extends Kunde>>(kunden){};
@@ -180,8 +202,8 @@ public class KundeResource {
 		}
 		
 		return Response.ok(entity)
-                       .links(links)
-                       .build();
+		               .links(links)
+		               .build();
 	}
 	
 	private Link[] getTransitionalLinksKunden(List<? extends Kunde> kunden, UriInfo uriInfo) {
@@ -200,28 +222,61 @@ public class KundeResource {
 		return new Link[] { first, last };
 	}
 	
+	/**
+	 * IDs mit gleichem Praefix suchen
+	 * @param idPrefix Der gemeinsame Praefix
+	 * @return Collection der IDs mit gleichem Praefix
+	 */
+	@GET
+	@Path("/prefix/id/{id:[1-9][0-9]*}")
+	public Collection<Long> findIdsByPrefix(@PathParam("id") String idPrefix) {
+		final Collection<Long> ids = ks.findIdsByPrefix(idPrefix);
+		return ids;
+	}
+	
+	/**
+	 * Nachnamen mit gleichem Praefix suchen
+	 * @param nachnamePrefix Der gemeinsame Praefix
+	 * @return Collection der Nachnamen mit gleichem Praefix
+	 */
+	@GET
+	@Path("/prefix/nachname/{nachname}")
+	@Produces({ APPLICATION_JSON, TEXT_PLAIN + ";qs=0.75" })
+	public Collection<String> findNachnamenByPrefix(@PathParam("nachname") String nachnamePrefix) {
+		final Collection<String> nachnamen = ks.findNachnamenByPrefix(nachnamePrefix);
+		return nachnamen;
+	}
+
+	
+	/**
+	 * Mit der URL /kunden/{id}/bestellungen die Bestellungen zu eine Kunden ermitteln
+	 * @param kundeId ID des Kunden
+	 * @return Objekt mit Bestellungsdaten, falls die ID vorhanden ist
+	 */
 	@GET
 	@Path("{id:[1-9][0-9]*}/bestellungen")
 	public Response findBestellungenByKundeId(@PathParam("id") Long kundeId) {
-		final Kunde kunde = ks.findKundeById(kundeId);
+		final Kunde kunde = ks.findKundeById(kundeId, FetchType.MIT_BESTELLUNGEN);
+		
 		final List<Bestellung> bestellungen = bs.findBestellungenByKunde(kunde);
 		// URIs innerhalb der gefundenen Bestellungen anpassen
 		if (bestellungen != null) {
 			for (Bestellung bestellung : bestellungen) {
 				bestellungResource.setStructuralLinks(bestellung, uriInfo);
 			}
-			// FIXME JDK 8 hat Lambda-Ausdruecke, aber Proxy-Klassen von Weld funktionieren noch nicht mit Lambda-Ausdruecken
+			// FIXME JDK 8 hat Lambda-Ausdruecke
 			//bestellungen.parallelStream()
-			//            .forEach(b -> bestellungResource.setStructuralLinks(b, uriInfo));0+,m
+			//            .forEach(b -> bestellungResource.setStructuralLinks(b, uriInfo));
 		}
 		
-		return Response.ok(new GenericEntity<List<Bestellung>>(bestellungen){})
-                       .links(getTransitionalLinksBestellungen(bestellungen, kunde, uriInfo))
-                       .build();
+		final Response response = Response.ok(new GenericEntity<List<Bestellung>>(bestellungen) {})
+                                          .links(getTransitionalLinksBestellungen(bestellungen, kunde, uriInfo))
+                                          .build();
+		return response;
 	}
 	
 	private Link[] getTransitionalLinksBestellungen(List<Bestellung> bestellungen, Kunde kunde,
-	                                                UriInfo uriInfo) {
+			                                        UriInfo uriInfo) {
 		if (bestellungen == null || bestellungen.isEmpty()) {
 			return new Link[0];
 		}
@@ -242,30 +297,111 @@ public class KundeResource {
 		return new Link[] { self, first, last };
 	}
 	
-	@POST
-	@Consumes({ APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
-	@Produces
-	public Response createKunde(@Valid Kunde kunde) {
-		// Rueckwaertsverweis von Adresse zu Kunde setzen
-		kunde.getAdresse().setKunde(kunde);
+
+	/**
+	 * Bestellung-IDs zu einem Kunden suchen
+	 * @param kundeId ID des Kunden
+	 * @return Liste der Bestellung-IDs
+	 */
+	@GET
+	@Path("{id:[1-9][0-9]*}/bestellungenIds")
+	@Produces({ APPLICATION_JSON, TEXT_PLAIN + ";qs=0.75", APPLICATION_XML + ";qs=0.5" })
+	public Response findBestellungenIdsByKundeId(@PathParam("id") Long kundeId) {
+		final Kunde kunde = ks.findKundeById(kundeId, FetchType.MIT_BESTELLUNGEN);
+
+		final Collection<Bestellung> bestellungen = bs.findBestellungenByKunde(kunde);		
+		final int anzahl = bestellungen.size();
+		final Collection<Long> bestellungenIds = new ArrayList<>(anzahl);
+		for (Bestellung bestellung : bestellungen) {
+			bestellungenIds.add(bestellung.getId());
+		}
+		// FIXME JDK 8 hat Lambda-Ausdruecke
+		//bestellungen.parallelStream()
+		//            .map(Bestellung::getId)
+		//            .forEach(id -> bestellungenIds.add(id));
 		
-		kunde = ks.createKunde(kunde);
-		
-		return Response.created(getUriKunde(kunde, uriInfo))
-			           .build();
+		return Response.ok(new GenericEntity<Collection<Long>>(bestellungenIds) {})
+				       .build();
 	}
 	
+	/**
+	 * Mit der URL /kunden/{id}/wartungsvertraege die Wartungsvertraege ermitteln
+	 * zu einem bestimmten Kunden ermitteln
+	 * @param id ID des Kunden
+	 * @return Wartungsvertraege, falls die ID vorhanden ist
+	 */
+	@GET
+	@Path("{id:[1-9][0-9]*}/wartungsvertraege")
+	public Response findWartungsvertraegeByKundeId(@PathParam("id") Long id) {
+		// Diese Methode ist bewusst NICHT implementiert, um zu zeigen,
+		// wie man Methodensignaturen an der Schnittstelle fuer andere
+		// Teammitglieder schon mal bereitstellt, indem einfach ein "Internal
+		// Server Error (500)" produziert wird.
+		// Die Kolleg/inn/en koennen nun weiterarbeiten, waehrend man selbst
+		// gerade keine Zeit hat, weil andere Aufgaben Vorrang haben.
+		
+		// TODO findWartungsvertraegeByKundeId noch nicht implementiert
+		return Response.status(INTERNAL_SERVER_ERROR)
+				       .entity("findWartungsvertraegeByKundeId: NOT YET IMPLEMENTED")
+				       .type(TEXT_PLAIN)
+				       .build();
+	}
+	
+
+	/**
+	 * Mit der URL /kunden einen Privatkunden per POST anlegen.
+	 * @param kunde neuer Kunde
+	 * @return Response-Objekt mit URL des neuen Privatkunden
+	 */
+	@POST
+	@Consumes({APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
+	@Produces
+	public Response createKunde(@Valid Kunde kunde) {
+		kunde.setId(KEINE_ID);
+		
+		final Adresse adresse = kunde.getAdresse();
+		if (adresse != null) {
+			adresse.setKunde(kunde);
+		}
+		
+		kunde = ks.createKunde(kunde);
+		LOGGER.tracef("Kunde: %s", kunde);
+		
+		return Response.created(getUriKunde(kunde, uriInfo))
+				       .build();
+	}
+	
+	
+	/**
+	 * Mit der URL /kunden einen Kunden per PUT aktualisieren
+	 * @param kunde zu aktualisierende Daten des Kunden
+	 */
 	@PUT
 	@Consumes({ APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
 	@Produces
 	public void updateKunde(@Valid Kunde kunde) {
-		ks.updateKunde(kunde);
+		// Vorhandenen Kunden ermitteln
+		final Kunde origKunde = ks.findKundeById(kunde.getId(), FetchType.NUR_KUNDE);
+		LOGGER.tracef("Kunde vorher: %s", origKunde);
+	
+		// Daten des vorhandenen Kunden ueberschreiben
+		origKunde.setValues(kunde);
+		LOGGER.tracef("Kunde nachher: %s", origKunde);
+		
+		// Update durchfuehren
+		ks.updateKunde(origKunde);
 	}
 	
+	
+	/**
+	 * Mit der URL /kunden{id} einen Kunden per DELETE l&ouml;schen
+	 * @param kundeId des zu l&ouml;schenden Kunden
+	 */
+	@Path("{id:[0-9]+}")
 	@DELETE
-	@Path("{id:[1-9][0-9]*}")
 	@Produces
 	public void deleteKunde(@PathParam("id") Long kundeId) {
-		ks.deleteKunde(kundeId);
+		final Kunde kunde = ks.findKundeById(kundeId, FetchType.NUR_KUNDE);
+		ks.deleteKunde(kunde);
 	}
 }
