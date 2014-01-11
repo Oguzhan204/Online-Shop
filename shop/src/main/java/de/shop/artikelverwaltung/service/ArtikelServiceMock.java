bc65d03c27b40f505b920ac32ea8efde00094578
Alex Stankewitsch <stal1017(at)hs-karlsruhe.de>
2014-01-11 13:03:28 +0100

    artikel service db

--- shop/src/main/java/de/shop/artikelverwaltung/service/ArtikelServiceMock.java
+++ shop/src/main/java/de/shop/artikelverwaltung/service/ArtikelServiceMock.java
@@ -1,15 +1,18 @@
 package de.shop.artikelverwaltung.service;
 
+import static javax.interceptor.Interceptor.Priority.APPLICATION;
+
+import javax.annotation.Priority;
+
 import de.shop.artikelverwaltung.domain.Artikel;
 import de.shop.util.cdi.MockService;
 import de.shop.util.interceptor.Log;
-import javax.enterprise.context.Dependent;
 
 /**
- * @author <a href="mailto:oguzhan.atmaca@web.de">Oguzhan Atmaca</a>
+ * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
  */
 @MockService
-@Dependent
+@Priority(APPLICATION + 100)
 @Log
 public class ArtikelServiceMock extends ArtikelService {
 	private static final long serialVersionUID = -2919310633845009282L;
@@ -21,7 +24,7 @@ public class ArtikelServiceMock extends ArtikelService {
 	public Artikel findArtikelById(Long id) {
 		final Artikel artikel = new Artikel();
 		artikel.setId(id);
-		artikel.setBezeichnung("Bezeichnung_" + id + "_Mock");
+		artikel.setBezeichnung("Bezeichnung" + id);
 		return artikel;
 	}
 }
